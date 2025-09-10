4559a2cdb91d35283760b475130b39ca1e62482d
Anandhu Anilkumar <anandhuanilkumar310@gamil.com>
2025-09-10 20:09:37 +0530

    Subscription with async pipe

--- src/app/widgets/display/display.ts
+++ src/app/widgets/display/display.ts
@@ -1,33 +1,35 @@
-import { TitleCasePipe } from '@angular/common';
+import { AsyncPipe, TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonService } from '../../services/common';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-display',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, AsyncPipe],
   templateUrl: './display.html',
   styleUrl: './display.scss'
 })
 
 export class DisplayComponent implements OnInit {
   public gridHeaders: any[] = [];
-  public gridData: GridData[] = [];
+  public gridData$!: Observable<GridData[]>;
 
-  constructor(private _common: CommonService, private _router: Router) {}
+  constructor(private _common: CommonService, private _router: Router) { }
 
   ngOnInit(): void {
     this.fetchData();
   }
 
   fetchData() {
-    this._common.getUsers().subscribe((res: GridData[]) => {
-      this.gridData = res;
-      if (res.length) {
-        this.gridHeaders = Object.keys(res[0]);
-      }
-    });
+    this.gridData$ = this._common.getUsers().pipe(
+      tap(res => {
+        if (res.length) {
+          this.gridHeaders = Object.keys(res[0]);
+        }
+      })
+    );
   }
 
   goToDetail(index: number) {
