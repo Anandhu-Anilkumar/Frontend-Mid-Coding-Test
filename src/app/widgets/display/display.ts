4b2f5ba43de9e4d675b8df93a0819647bcc02e12
Anandhu Anilkumar <anandhuanilkumar310@gamil.com>
2025-09-10 21:17:56 +0530

    Table Inline Editing

--- src/app/widgets/display/display.ts
+++ src/app/widgets/display/display.ts
@@ -2,7 +2,7 @@ import { AsyncPipe, TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonService } from '../../services/common';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-display',
@@ -16,6 +16,10 @@ export class DisplayComponent implements OnInit {
   public gridHeaders: any[] = [];
   public gridData$!: Observable<GridData[]>;
 
+  private editingCell: { rowIndex: number, field: string } | null = null;
+  private editableFields = ['name', 'language', 'bio', 'version'];
+  private dataSubject = new BehaviorSubject<GridData[]>([]);
+
   constructor(private _common: CommonService, private _router: Router) { }
 
   ngOnInit(): void {
@@ -23,13 +27,37 @@ export class DisplayComponent implements OnInit {
   }
 
   fetchData() {
-    this.gridData$ = this._common.getUsers().pipe(
+    this._common.getUsers().pipe(
       tap(res => {
         if (res.length) {
           this.gridHeaders = Object.keys(res[0]);
+          this.dataSubject.next([...res]);
         }
       })
-    );
+    ).subscribe();
+
+    this.gridData$ = this.dataSubject.asObservable();
+  }
+
+  startEdit(rowIndex: number, field: string) {
+    this.editingCell = { rowIndex, field };
+  }
+
+  saveEdit(rowIndex: number, field: string, newValue: string) {
+    const currentData = this.dataSubject.value;
+    if (currentData[rowIndex]) {
+      currentData[rowIndex][field] = newValue;
+      this.dataSubject.next([...currentData]);
+    }
+    this.editingCell = null;
+  }
+
+  isEditing(rowIndex: number, field: string): boolean {
+    return this.editingCell?.rowIndex === rowIndex && this.editingCell?.field === field;
+  }
+
+  isEditable(field: string): boolean {
+    return this.editableFields.includes(field.toLowerCase());
   }
 
   goToDetail(index: number, data: GridData) {
